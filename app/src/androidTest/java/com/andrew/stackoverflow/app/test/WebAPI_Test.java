9c8d994c749b07776cd314e4d40b8cd981c45ade
andrewmelis <andrewmelis@gmail.com>
2014-06-19 03:04:45 -0500

    adding square mockwebserver library for http testing

---
+++ app/src/androidTest/java/com/andrew/stackoverflow/app/test/WebAPI_Test.java
@@ -0,0 +1,9 @@
+package com.andrew.stackoverflow.app.test;
+
+import android.test.AndroidTestCase;
+
+public class WebAPI_Test extends AndroidTestCase {
+
+    
+
+}
---
+++ app/src/main/java/com/andrew/stackoverflow/app/WebAPI.java
@@ -0,0 +1,12 @@
+package com.andrew.stackoverflow.app;
+
+public class WebAPI {
+
+    public static final String queryString = "https://api.stackexchange.com/2.2/questions?pagesize=1&order=desc&sort=activity&site=stackoverflow";
+
+    public String performFetchQuestion() {
+
+        return null;
+    }
+
+}
